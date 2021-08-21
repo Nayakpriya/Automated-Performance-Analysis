e06b6c0bf9ec307367130cff73221498c646ac21
Nayakpriya <priyanayak.cs19@rvce.edu.in>
2021-08-21 11:02:30 +0530

    forms

---
+++ student/views.py
@@ -0,0 +1,79 @@
+
+from .models import Question,Marks
+from django.contrib import messages,auth
+from django.shortcuts import redirect, render, get_object_or_404
+
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import Questionform
+from .forms import Marksform
+
+def home(request):
+    return render(request,'home.html')
+
+def question_add(request):
+    c_form = Questionform()
+
+    if request.POST:
+        c_form = Questionform(request.POST)
+        if c_form.is_valid():
+            c_form.save()
+            messages.success(request,'The question is added')
+            return redirect('question-add')
+    return render(request, 'create.html', context={'form':c_form, 'button_text': 'Add Question'})
+
+
+def login(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=name, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Logged in as a student')
+            
+            return redirect('question-add')
+        else:
+            messages.warning(request, 'invalid credentials')
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def marks_add(request):
+    c_form = Marksform()
+
+    if request.POST:
+        c_form = Marksform(request.POST)
+        if c_form.is_valid():
+            c_form.save()
+            messages.success(request,'The question is added')
+            return redirect('question-add')
+    return render(request, 'create.html', context={'form':c_form, 'button_text': 'Add Student Details'})
+
+
+def marks_details(request):
+    marks=Marks.objects.all()
+    context={
+        'title':'Marks Details',
+        'data':marks
+    }
+    return render(request,'table.html',context=context)
+
+
+def marks_charts(request):
+    labels=[]
+    data=[]
+
+    queryset=Marks.objects.order_by('total')
+    for mark in queryset:
+        labels.append(mark.student_name)
+        data.append(mark.total)
+        context={
+            'labels':labels,
+            'data':data
+        }
+        return render(request,'index.html',context=context)
